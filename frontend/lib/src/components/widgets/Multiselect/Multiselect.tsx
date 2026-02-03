8ec0c9e086a836beb305ad8eb8a112688a4a0d2c
Debbie Matthews <debbie.matthews@snowflake.com>
2026-02-02 22:09:38 -0800

    Style multiselect and selectbox

--- frontend/lib/src/components/widgets/Multiselect/Multiselect.tsx
+++ frontend/lib/src/components/widgets/Multiselect/Multiselect.tsx
@@ -372,8 +372,7 @@ const Multiselect: FC<Props> = props => {
                   Body: {
                     style: () => ({
                       maxHeight: "70vh",
-                      // Scrolling is handled by the VirtualDropdown component
-                      overflow: "hidden",
+                      overflow: "auto",
                       boxSizing: "border-box",
 
                       borderTopLeftRadius: theme.radii.default,
